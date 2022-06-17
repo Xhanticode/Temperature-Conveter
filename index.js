f12ee6d2a35f07fdb343c9bf0cc4a777d36ea234
Xhanticode <xhantimpu@gmail.com>
2022-06-17 16:20:28 +0200

    fourth push

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ let fahrenheit = document.querySelector("#fahrenheit");
 
 function cConverter(valNum) {
   valNum = parseFloat(valNum);
-  fahrenheit.value = eval(`${valNum} * 1.8 + 32`);
-  kelvin.value = eval(`${valNum} + 273.15`);
+  fahrenheit.value = eval(`${valNum} * 1.8 + 32`).toFixed(2);
+  kelvin.value = eval(`${valNum} + 273.15`).toFixed(2);
 }
 
 // Fahrenheit to Celcius and Kelvin
 function fConverter(valNum) {
   valNum = parseFloat(valNum);
-  celcius.value = eval(`(${valNum} - 32) / 1.8`);
-  kelvin.value = eval(`(${valNum} - 32) / 1.8 + 273.15`);
+  celcius.value = eval(`(${valNum} - 32) / 1.8`).toFixed(2);
+  kelvin.value = eval(`(${valNum} - 32) / 1.8 + 273.15`).toFixed(2);
 }
 
 // Kelvin to Celcius and Fahrenheit
 function kConverter(valNum) {
   valNum = parseFloat(valNum);
-  fahrenheit.value = eval(`1.8 * (${valNum} - 273) + 32`);
-  celcius.value = eval(`${valNum} - 273.15`);
+  fahrenheit.value = eval(`1.8 * (${valNum} - 273) + 32`).toFixed(2);
+  celcius.value = eval(`${valNum} - 273.15`).toFixed(2);
 }
 
 // Reset
